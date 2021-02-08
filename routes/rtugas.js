aa9904da61c9511dec42bf062c95019584e738cd
Teguh Firmansyah <teguhf522@gmail.com>
2021-02-08 13:17:45 +0700

    api detail tugas & historytugas

--- routes/rtugas.js
+++ routes/rtugas.js
@@ -22,13 +22,27 @@ route.route('/available').get(async (req, res, next) => {
         next(Error(error))
     }
 })
-route.route('/detail/:id_create_tugas?/:customer_id?').get(async (req, res, next) => {
+route.route('/detail/:id_tugas?').get(async (req, res, next) => {
     try {
         const listugas = await db.frm_create_tugas.findAll({
             where: {
-                id_create_tugas: req.query.id_create_tugas
+                id_create_tugas: req.query.id_tugas
             }
         })
+        res.json({ data: listugas })
+    } catch (error) {
+        next(Error(error))
+    }
+})
+route.route('/historytugasuser/:customer_id?').get(async (req, res, next) => {
+    try {
+        const listugas = await db.sequelize.query(`SELECT * FROM frm_usr_tugas as a
+        left join frm_create_tugas as b on a.id_tugas=b.id_create_tugas 
+        where id_auth='${req.query.customer_id}'
+        ORDER By id_create_tugas DESC
+        ` , {
+            type: db.sequelize.QueryTypes.SELECT
+        });
         res.json({ data: listugas })
     } catch (error) {
         next(Error(error))
